b01c3eafcd790319fc4b4763e42b627d6bc9af41
Icaro <icaronunes@hotmail.com>
2019-05-30 00:15:57 -0300

    inicio da estrutura

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
-import { AddEntry} from './components/AddEntry'
+import  AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
+  
   render() {
     return (
-      <View >
+      <View>
         <AddEntry />
       </View> 
     )
